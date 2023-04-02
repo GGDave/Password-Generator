88f498cdf43ee9590873b6a559492b2e85e5191f
David Flores <dcflores@live.com>
2023-04-02 15:08:39 -0700

    changed variables from strings to arrays

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-var upperCase = lowerCase.toLocaleUpperCase();
-var numeric = '0123456789'
-var specialcharacters = '!@#$%&*?'
 
 // Write password to the #password input
 function writePassword() {
@@ -12,19 +7,23 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 // WHEN I click the button to generate a password
 generateBtn.addEventListener("click", writePassword);
 
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numeric = ["0","1","2","3","4","5","6","7","8","9"];
+var specialChar =["!","@","#","$","%","&","*","?"] 
+var password = "";
 
 function generatePassword() {
-    var password = "";
-   // THEN I am presented with a series of prompts for password criteria
-   
-   // THEN I select which criteria to include in the password
-   // WHEN prompted for the length of the password
-   // THEN I choose a length of at least 8 characters and no more than 128 characters
+  
+  // THEN I am presented with a series of prompts for password criteria
+  
+  // THEN I select which criteria to include in the password
+  // WHEN prompted for the length of the password
+  // THEN I choose a length of at least 8 characters and no more than 128 characters
    const passwordLenght = prompt ("How long would you like your password? 8-128 characters");
    
    // WHEN asked for character types to include in the password
@@ -33,7 +32,9 @@ function generatePassword() {
    var upperCase = confirm("Do you want uppercase?");
    var numeric = confirm("Do you want numeric?");
    var specialcharacters = confirm("Do you want special characters?");
-
+  //pasword lenth varification, verifies that the number that was inputed was a number between 8-128
+   
+  }
    // THEN my input should be validated and at least one character type should be selected
    if(lowercase){
     password = password + getRandomValue(lowerCase);
